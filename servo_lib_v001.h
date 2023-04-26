445ee138a163b9e835517a4e8694ede640726207
isaguilfoile <118629718+isaguilfoile@users.noreply.github.com>
2023-04-26 10:42:12 -0500

    Add files via upload

---
+++ servo_lib_v001.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   servo_lib_v001.h
+ * Author: isaiahguilfoile
+ *
+ * Created on April 20, 2023, 4:27 PM
+ */
+
+#ifndef SERVO_LIB_V001_H
+#define	SERVO_LIB_V001_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    void initServo(void);
+    void setServo(int val);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SERVO_LIB_V001_H */
+
